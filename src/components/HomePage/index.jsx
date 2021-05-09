b354042b8cdb137359b0beb09c0f3b5dc5e44790
mubangizi <mubangizia22@gmail.com>
2021-05-10 00:39:31 +0300

    Add homepage layout

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Home.css";
 import Layout from "../Layout";
+import SideBar from "../SideBar";
 
 function Home() {
   return (
     <Layout>
-      <div className="Container">
-        <div className="HomeMainContent">Hello</div>
+      <div className="ContentContainer">
+        <div className="HomeMainContent">
+          <SideBar />
+          <div className="InfoSection ContentItem">
+            <h1>Wallet</h1>
+            <div className="BalanceInfo">
+              <div className="Balance">Balance:</div>
+              <div className="BalanceValue">5,000,000</div>
+              <div className="BalanceCurrency">USD</div>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
