d02c08be904941f4592bd5143a63add4b145ce9e
mubangizi <mubangizia22@gmail.com>
2021-05-10 02:34:03 +0300

    Fix fail build

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./SideBar.css";
-import { Send } from "@material-ui/icons";
 
 const SideBar = () => {
-  const [open, setOpen] = React.useState(false);
-  const history = useHistory();
-
-  const handleDrawer = () => {
-    if (open === true) {
-      setOpen(false);
-    } else {
-      setOpen(true);
-    }
-  };
-
-  const sendToHome = () => {
-    history.push("/");
-  };
   return (
     <div className="SideBar">
-      <div className={`SideBarContent Side-${open}`}>
+      <div className={`SideBarContent `}>
         <div className="NavItems">
           <NavLink to={{ pathname: `/` }} exact={true}>
             Dashboard
@@ -29,16 +14,6 @@ const SideBar = () => {
           <NavLink to={{ pathname: `/deposit` }}>Deposit</NavLink>
         </div>
       </div>
-      {/* <div className="SideControllerBtn">
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawer}
-          edge="start"
-        >
-          {!open ? <MenuOutlinedIcon /> : <CloseOutlinedIcon />}
-        </IconButton>
-      </div> */}
     </div>
   );
 };
